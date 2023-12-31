b31c36e8751140859b21d9220fba9ad7c3727616
Ritik <ritik221jaggi@gmail.com>
2023-12-31 23:47:45 +0530

    done

--- src/Components/Withdrawl/Withdrawl.jsx
+++ src/Components/Withdrawl/Withdrawl.jsx
@@ -1,10 +1,31 @@
-import { React, useEffect, useState} from 'react'
+import { React, useEffect, useState } from 'react'
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import logo from './../../assets/bank.png'
 import './Withdrawl.scss'
 import Lottie from 'react-lottie';
-import animationData from './../../assets/cash.json'; 
+import animationData from './../../assets/cash.json';
+import './../Login/Loginpage';
 const Withdrawl = () => {
+
+  const [amount, setAmount] = useState('');
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const CancelButton = () => {
+    navigate('/')
+  }
+  const handleConfirm = () => {
+    const parsedAmount = parseFloat(amount);
+
+    if (parsedAmount <= 0 || isNaN(parsedAmount)) {
+      alert('Amount must be a positive number');
+      setAmount(''); // Reset the input field
+    } else {
+      // Handle the confirmation logic here
+      alert(`Transaction Done of amount: ${amount} \n Please collect your cash. \n **Thank you for visiting National Bank**`);
+    }
+  } 
   const location = useLocation();
   const defaultOptions = {
     loop: true,
@@ -20,40 +41,44 @@ const Withdrawl = () => {
   const [showDiv, setShowdiv] = useState(true)
   const navigate = useNavigate()
 
-    return (
-      <div className='container3'>
-        <div className="top">
-          <img src={logo} alt="" />
-          <div className="user">{location.state.Data},</div>
-        </div>
-        <div className="heading">
-          <h2>Withdrawl</h2>
-        </div>
-        <div className="annimation">
-          <Lottie options={defaultOptions} height={250} width={550} />
-        </div>
-      
-        {showDiv ? (
-          <div className="middle">
-            <div className='left'>
-              <button onClick={handleCurrent}>Current</button>
-            </div>
-            <div className='right'>
-              <button onClick={handleSaving}>Savings</button>
-            </div>
-          </div>
-        ) : 
-          <div className="inputs">
-            <h2>Enter the amount</h2>
-            <input type="number"
-             />
-            <div className="buttons">
-              <button>Cancel</button>
-              <button >Confirm</button>
-            </div>
-        </div>
-        } 
+  return (
+    <div className='container3'>
+      <div className="top">
+        <img src={logo} alt="" />
+        <div className="user">{location.state.Data},</div>
       </div>
-    )
-  }
- export default Withdrawl
+      <div className="heading">
+        <h2>Withdrawl</h2>
+      </div>
+      <div className="annimation">
+        <Lottie options={defaultOptions} height={250} width={550} />
+      </div>
+
+      {showDiv ? (
+        <div className="middle">
+          <div className='left'>
+            <button onClick={handleCurrent}>Current</button>
+          </div>
+          <div className='right'>
+            <button onClick={handleSaving}>Savings</button>
+          </div>
+        </div>
+      ) :
+        <div className="inputs">
+          <h2>Enter the amount</h2>
+          <label >
+            <input type="number"
+              value={amount}
+              onChange={handleAmountChange}
+            />
+          </label>
+          <div className="buttons">
+            <button onClick={CancelButton}>Cancel</button>
+            <button onClick={handleConfirm}>Confirm</button>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
+export default Withdrawl
